06088665acf6875407c6d80cd7a89e5e770f5e4a
appl11361 <apple113611361@gmail.com>
2017-10-25 10:48:35 +0800

    receive GPS information from MP

--- Assets/script/cube.cs
+++ Assets/script/cube.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using System.IO;
 using System.IO.Ports;
@@ -8,34 +9,54 @@ using System.Threading;
 
 public class cube : MonoBehaviour {
 
-    public float speed;             //移動速度
-    public GameObject bullet;       //子彈
-    private bool which_gun;         //選槍
-    private MeshRenderer mr;        //cube外觀
+    public float speed;                     //移動速度
+    public GameObject bullet;               //子彈
+    private bool which_gun;                 //選槍
+    private MeshRenderer mr;                //cube外觀
+    private string GPS_data;                //GPS資料
+    private string GPS_information = "";    //parse後的需要資訊
+    private double longitude = 0;           //經度
+    private double latitude = 0;            //緯度
 
     /****************variable about com port****************/
-    const int dollar_sign = 36;         //$
-    const int carriage_return = 13;     //CR
-    const int line_feed = 10;           //LF
+    /*const int dollar_sign = 36;         //$
+    /*const int carriage_return = 13;     //CR
+    /*const int line_feed = 10;           //LF
+    /*
+    /*private bool receiving;             //decide if execute DoReceive()         
+    /*private SerialPort comport;
+    /*private Thread t;
+    *********************************************************/
 
-    private bool receiving;         //decide if execute DoReceive()         
+    /********************receive mavlink********************/
+    MAVLink.MavlinkParse mavlink = new MAVLink.MavlinkParse();
+    MAVLink.MAVLinkMessage packet;
     private SerialPort comport;
-    private Thread t;
-    
+    object readlock = new object();     // locking to prevent multiple reads on serial port
+
 
     // Use this for initialization
     void Start ()
     {
-        speed = 3;
+        speed = 10;
         which_gun = false;
 
         mr = GetComponent<MeshRenderer>();
         mr.material.color = Color.red;
 
         /***********setting about com port***********/
-        receiving = true;
+        //receiving = true;
 
-        comport = new SerialPort("COM5", 9600, Parity.None, );
+        /*comport = new SerialPort("COM5", 9600, Parity.None, 8, StopBits.One);
+        if(!comport.IsOpen)
+        {
+            comport.Open();
+            comport.ReadTimeout = 2000;     // set timeout to 2 seconds
+
+            //t = new Thread(DoReceive);
+            //t.IsBackground = true;
+            //t.Start();
+        }*/
     }
 
     // Update is called once per frame
@@ -130,41 +151,84 @@ public class cube : MonoBehaviour {
     }
 
     //serial port communication for GPS
-    void DoReceive()
+    /*void DoReceive()
     {
-        List<Byte> tempList = new List<Byte>();     //store receivedValue temporarily until CR LF
+        List<Byte> tempList = new List<Byte>();         //store receivedValue temporarily until CR LF
         int receivedValue;
 
         while (receiving)
         {
-            if (comport.BytesToRead > 0)
+            receivedValue = comport.ReadByte();
+            switch (receivedValue)
             {
-                receivedValue = comport.ReadByte();
+                case dollar_sign:
+                    tempList.Clear();
+                    tempList.Add((Byte)receivedValue);
+                    break;
 
-                switch (receivedValue)
-                {
-                    case dollar_sign:
-                        tempList.Clear();
-                        tempList.Add((Byte)receivedValue);
-                        break;
+                case carriage_return:                   //for computer test
+                    tempList.Add((Byte)receivedValue);
+                    parse(tempList);
+                    break;
 
-                    case line_feed:
-                        tempList.Add((Byte)receivedValue);
+                case line_feed:                         //for GPS input
+                    tempList.Add((Byte)receivedValue);
+                    parse(tempList);
+                    break;
 
-                        break;
+                case -1:
+                    break;
 
-                    case -1:
-                        break;
-
-                    default:
-                        tempList.Add((Byte)receivedValue);
-                        break;
-                }
+                default:
+                    tempList.Add((Byte)receivedValue);
+                    break;
             }
-            else
+        }
+    }*/
+
+    void parse(List<Byte> tempList)
+    {
+        string header = System.Text.ASCIIEncoding.ASCII.GetString(tempList.ToArray(), 0, 6);
+        //string pat = @"\w+,\w+,([0-9]*.[0-9]*,\w),([0-9]*.[0-9]*,\w),\w+";
+        string pat = @".*,([0-9]*\.[0-9]*),[S,N].*,([0-9]*\.[0-9]*),[E,W].*";
+        Regex r = new Regex(pat, RegexOptions.IgnoreCase);
+        Match m;
+
+        print(System.Text.ASCIIEncoding.ASCII.GetString(tempList.ToArray()));
+        if (header == "$GPGGA")
+        {
+            GPS_data = System.Text.ASCIIEncoding.ASCII.GetString(tempList.ToArray());
+            m = r.Match(GPS_data);
+
+            print(GPS_data);
+            print(m.Groups[1]);
+            print(m.Groups[2]);
+
+            if (m.Success)
             {
-                Thread.Sleep(16);
+               //longitude = Convert.ToDouble(m.Groups[2]);      //經度，英文好難QQ
+               //latitude = Convert.ToDouble(m.Groups[1]);       //緯度，英文好難QQ
+
+                GPS_information = "";
+                GPS_information += (m.Groups[1] + "\n");
+                GPS_information += m.Groups[2];
             }
         }
     }
+
+    void OnGUI()
+    {
+        GUI.TextArea(new Rect(2, 50, 200, 50), GPS_information);
+        GUI.TextArea(new Rect(2, 100, 200, 50), longitude.ToString());
+    }
+
+    private void OnApplicationQuit()
+    {
+        //receiving = false;
+        comport.DiscardInBuffer();
+        comport.Close();
+        //t.Abort();
+    }
+
+
 }
